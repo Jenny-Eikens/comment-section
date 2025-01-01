455b46b29b84b7ad2d7e70ee9ec4e05ff107a61a
Jenny-Eikens <jennifereikens@web.de>
2025-01-01 19:44:33 +0100

    Add hover effect to voting

--- app/components/Comment.tsx
+++ app/components/Comment.tsx
@@ -8,6 +8,7 @@ const iconPlus = (
     <path
       d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149.099-.1.148-.217.148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15c-.1.099-.149.217-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z"
       fill="#C5C6EF"
+      className="transition-colors group-hover:fill-primary"
     />
   </svg>
 );
@@ -17,6 +18,7 @@ const iconMinus = (
     <path
       d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z"
       fill="#C5C6EF"
+      className="transition-colors group-hover:fill-primary"
     />
   </svg>
 );
@@ -139,7 +141,7 @@ const Comment = ({
         {/* Voting */}
         <div className="voting flex flex-row items-center justify-between rounded-lg bg-base-100 p-2 md:mr-2 md:h-[6rem] md:w-auto md:flex-col md:py-3">
           <button
-            className="p-1"
+            className="group p-1"
             onClick={() => onClickPlus(comment.id)}
             disabled={isUser}
             aria-label="Plus one"
@@ -148,7 +150,7 @@ const Comment = ({
           </button>
           <span className="font-[700] text-primary">{comment.score}</span>
           <button
-            className="p-1"
+            className="group p-1"
             onClick={() => onClickMinus(comment.id)}
             disabled={isUser}
             aria-label="Minus one"
